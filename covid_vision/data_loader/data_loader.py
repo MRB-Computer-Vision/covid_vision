7f75b232c702a2767a5807da4fa3a3107d80fdb8
Edson Cavalcanti <edsoncavalcanti@mobitbrasil.com.br>
2020-05-12 08:23:06 -0300

    modification on the data loader

--- covid_vision/data_loader/data_loader.py
+++ covid_vision/data_loader/data_loader.py
@@ -1,36 +1,33 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
-from covid_vision.utils.file_manipulation import make_dir, list_files
-import zipfile
+from covid_vision.utils.file_manipulation import make_dir, list_files, download_from_google_drive
 import cv2
 
 
 class DataLoader:
-    def __init__(self, type):
-        self.images = {}
+
+    def __init__(self, data_type):
+        self.images = dict()
         self.images['covid'] = []
         self.images['non-covid'] = []
-        self.type = type
+        self.data_type = data_type
 
-    def download_xray_datasets(self):
+    def _download_xray_datasets(self):
+        """Download the XRay dataset from Google"""
         file_id = "1SG5PorcAdZvTrp6KbivSHQv4TKgMqusW"
-        self.download_from_google_drive(file_id=file_id, folder="data/", name="dataset_covid_19.zip")
-
-    def download_from_google_drive(self, file_id, folder, name):
-        """
-        Download de um arquivo do google drive
-        @param file_id identificador do google drive do arquivo
-        @param folder pasta que será armazenado
-        @param name nome do arquivo que será salvo
-        """
-        make_dir(folder)
-        gdd.download_file_from_google_drive(file_id=file_id, dest_path=folder + name, showsize=True)
-        with zipfile.ZipFile(folder + name, 'r') as zip_ref:
-            zip_ref.extractall(folder)
+        download_from_google_drive(file_id=file_id, folder="data/", name="dataset_covid_19.zip")
 
     def load_dataset(self):
-        if self.type is "xray":
+
+        if self.data_type is "xray":
+            if list_files("data/Dataset COVID-19 Augmented"):
+                self._download_xray_datasets()
             names_covid = list_files("data/Dataset COVID-19 Augmented/COVID-19")
             names_noncovid = list_files("data/Dataset COVID-19 Augmented/Non-COVID-19")
+        elif self.data_type is "ultrassound":
+            raise NotImplementedError
+        elif self.data_type is "computedtomography":
+            raise NotImplementedError
+        elif self.data_type is "anamnesis":
+            raise NotImplementedError
 
         for name in names_covid:
             self.images['covid'].append(cv2.imread(name))
--- covid_vision/utils/file_manipulation.py
+++ covid_vision/utils/file_manipulation.py
@@ -8,11 +8,22 @@ def make_dir(path):
     """
     Path(path).mkdir(parents=True, exist_ok=True)
 
+def download_from_google_drive(self, file_id, folder, name):
+    """
+    Download a file from Google
+    @param file_id identifier from google drive
+    @param folder where the file should be saved
+    @param name name of the file where the file will be saved
+    """
+    make_dir(folder)
+    gdd.download_file_from_google_drive(file_id=file_id, dest_path=folder + name, showsize=True)
+    with zipfile.ZipFile(folder + name, 'r') as zip_ref:
+        zip_ref.extractall(folder)
 
 def read_names(path):
     """
     Read the files with names of classes and returns one array with strings
-    @param path to the file
+    @param path path to the file
     """
     file = open(path, 'r')
     return file.readlines()
